eab55699f3d52c0eb90f4e15c2e0dc8916006fd2
Rusty <rusty@guysmily.com>
2018-10-31 20:09:09 -0700

    Add pin constants to UserInclude.h, convert user functions to c

--- UserInclude.h
+++ UserInclude.h
@@ -3,5 +3,17 @@
 #define USERINCLUDE_H_
 
 // TODO: add user code here
+#define PIN_DIO_LEFTBUMPER 1
+#define PIN_DIO_RIGHTBUMPER 2
+#define PIN_DIO_LIMITSWITCH 5
+#define PIN_MOTOR_LEFT 8
+#define PIN_MOTOR_RIGHT 9
+#define PIN_MOTOR_SERVO 2
+
+void Read_PD(void);
+int expose_and_read(void);
+void find_max(void);
+void move(void);
+int limit_pwm(int temp);
 
 #endif // USERINCLUDE_H_
