c983ebcea1d9be4735f5a39e8df3571d1fca5f0b
Rusty <rusty@guysmily.com>
2018-11-02 00:07:50 -0700

    Combine Read_PD.c, expode_and_read.c.  Clean up Read_PD()

--- Read_PD.c
+++ Read_PD.c
@@ -1,21 +1,31 @@
 #include "Main.h"
 
-void Read_PD ( void )
+void Read_PD(void)
 {
-      SetDigitalOutput ( 11 , 1 ) ; // close shutter, clear film, and increment counter
-      SetDigitalOutput ( 12 , 1 ) ; // initialize conter value to '0'
-      SetDigitalOutput ( 12 , 0 ) ; // aloww counter to count
-      Wait ( 5 ) ; // 5mS wait for things settling down
-      SetDigitalOutput ( 11 , 0 ) ; // open shutter and expose film
-      Wait ( expose_time  ) ; // exposer time = 3mS to 8mS
-      PD0 = GetAnalogInput ( 1 ) ; // read intensity
-      PD1 = expose_and_read ( ) ;
-      PD2 = expose_and_read ( ) ;
-      PD3 = expose_and_read ( ) ;
-      PD4 = expose_and_read ( ) ;
-      PD5 = expose_and_read ( ) ;
-      PD6 = expose_and_read ( ) ;
-      PD7 = expose_and_read ( ) ;
-      PD_sum=PD0+PD1+PD2+PD3+PD4+PD5+PD6+PD7;
+      PD0 = expose_and_read(true);
+      PD1 = expose_and_read(false);
+      PD2 = expose_and_read(false);
+      PD3 = expose_and_read(false);
+      PD4 = expose_and_read(false);
+      PD5 = expose_and_read(false);
+      PD6 = expose_and_read(false);
+      PD7 = expose_and_read(false);
+      PD_sum = PD0 + PD1 + PD2 + PD3 + PD4 + PD5 + PD6 + PD7;
 }
 
+int expose_and_read(bool reset_counter)
+{
+      int intensity;
+
+      SetDigitalOutput(11, 1); // close shuter, clear film, increment counter
+      if (reset_counter)
+      {
+            SetDigitalOutput(12, 1); // initialize conter value to '0'
+            SetDigitalOutput(12, 0); // aloww counter to count
+      }
+      Wait(5);                       // Wait 5mS for things to settle
+      SetDigitalOutput(11, 0);       // open shutter for exposure
+      Wait(expose_time);             // expose time = 3mS to 8mS (can be adjusted)
+      intensity = GetAnalogInput(1); // get the IR intensity reading
+      return intensity;
+}
