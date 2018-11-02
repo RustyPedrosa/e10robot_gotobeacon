1c150bf1bdc2b314e960c16e109e9438f0bcefff
Rusty <rusty@guysmily.com>
2018-11-01 23:41:23 -0700

    Add servo movement test and (broken) limit switch loop

--- UserInclude.h
+++ UserInclude.h
@@ -2,14 +2,21 @@
 #ifndef USERINCLUDE_H_
 #define USERINCLUDE_H_
 
-// TODO: add user code here
+/******************************************************************************
+ * Constants
+ *****************************************************************************/
+// Pin numbers ----------------------------------------
+// ADC pins
+// DIO pins
 #define PIN_DIO_LEFTBUMPER 1
 #define PIN_DIO_RIGHTBUMPER 2
 #define PIN_DIO_LIMITSWITCH 5
+// Motor pins
 #define PIN_MOTOR_LEFT 8
 #define PIN_MOTOR_RIGHT 9
 #define PIN_MOTOR_SERVO 2
 
+void customMain(void);
 void Read_PD(void);
 int expose_and_read(void);
 void find_max(void);
--- customMain.c
+++ customMain.c
@@ -1,22 +1,24 @@
 #include "Main.h"
 
-void customMain ( void )
+void customMain(void)
 {
-      freq=0; // 0=1khz (red), 1=10kHz(green beacon)
-      ambient_level = 200; // esed in 'move'
-      slow_level = 5000; // used in 'move'
-      stop_level = 6000; // used in 'move'
-      expose_time = 3; // used in expose_and_read
-      steer_sensitivity = 20; // used in 'move'
-      forward_speed = 35; // forward speed, used in 'move'
-      slow_speed = 25; // slow speed, used in 'move'
-      spin_speed = 50; // spin speed (for searching mode), used in 'move'
-      SetDigitalOutput ( 10 , freq ) ; // turn to 1kHz (red beacon)
-      while ( 1==1 )
-      {
-            Read_PD (   ) ;
-            find_max ( ) ;
-            move ( ) ;
-      }
-}
+      freq = 0;                   // 0=1khz (red), 1=10kHz(green beacon)
+      ambient_level = 200;        // esed in 'move'
+      slow_level = 5000;          // used in 'move'
+      stop_level = 6000;          // used in 'move'
+      expose_time = 3;            // used in expose_and_read
+      steer_sensitivity = 20;     // used in 'move'
+      forward_speed = 35;         // forward speed, used in 'move'
+      slow_speed = 25;            // slow speed, used in 'move'
+      spin_speed = 50;            // spin speed (for searching mode), used in 'move'
+      SetDigitalOutput(10, freq); // turn to 1kHz (red beacon)
 
+      SetServo(PIN_MOTOR_SERVO, -127);
+      while (GetDigitalInput(5)) // while limit switch unpressed
+      {
+            Read_PD();
+            find_max();
+            move();
+      }
+      SetServo(PIN_MOTOR_SERVO, 120);
+}
